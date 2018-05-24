a3fe37ee0593850cef01e7760736d2776a3063b3
Daniel Martinez <dani@danielcastelao.org>
2018-05-24 14:09:15 +0200

    añadida base de datos.

--- src/comercio/Comercio.java
+++ src/comercio/Comercio.java
@@ -19,13 +19,18 @@ public class Comercio{
     public static void main(String[] args){
         
         Ventas[] array_ventas={new Ventas("1","rpro1",7),new Ventas("2","rpro2",9),new Ventas("3","rpro3",4)};
+        MetodosArray.anadirVentas(array_ventas);
         Producto[] array_productos={new Producto("rpro3","uvas","rpre1"),new Producto("rpro1","peras","rpre3"),new Producto("rpro2","patatas","rpre2")};
+        MetodosArray.anadirProducto(array_productos);
         Precio[] array_precios={new Precio("rpre2",2),new Precio("rpre1",5),new Precio("rpre3",7)};
+        MetodosArray.anadirPrecio(array_precios);
         Ticket[] array_ticket=new Ticket[3];
         
         for(int i=0;i<array_ticket.length;i++){
             array_ticket[i]= new Ticket();
         }
+        
+        MetodosArray.anadirTicket(array_ticket);
         
         MetodosArray.crear(array_ventas, array_productos, array_precios, array_ticket);
         MetodosArray.nombrePrecio(array_ventas, array_productos, array_precios);
--- src/comercio/MetodosArray.java
+++ src/comercio/MetodosArray.java
@@ -5,35 +5,116 @@
  */
 package comercio;
 
-import java.util.Arrays;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author dani
- */
-
-/*
-Sacar el nombre y precio del producto con el número de venta.
-*/
 public class MetodosArray{
-    
-    static String numeroVenta,referenciaProducto,referenciaPrecio,nombre;
-    static float precio,cantidad; 
-    
+
+    static Connection conexion;
+    static PreparedStatement st;
+    static ResultSet resulado;
+
+    static String numeroVenta, referenciaProducto, referenciaPrecio, nombre;
+    static float precio, cantidad;
+
+    public static void connect(){
+        try{
+            conexion=DriverManager.getConnection("jdbc:sqlite:"+"Comercio");
+            if(conexion!=null){
+                System.out.println("conectado");
+            }
+        }catch(SQLException ex){
+            System.out.println("Error al conectarse a la base.");
+        }
+    }
+
+    public static void close(){
+        try{
+            conexion.close();
+        }catch(SQLException ex){
+            System.out.println("Error al cerrar la base");
+        }
+    }
+
+    public static void anadirPrecio(Precio[] precio){
+        connect();
+
+        try{
+            for(int i=0; i<precio.length; i++){
+                conexion.prepareStatement("insert into Ticket values('"+precio[i].getReferenciaPrecio()+"'"
+                        +","+"'"+precio[i].getPrecio()+"'"+");");
+            }
+        }catch(SQLException ex){
+            System.out.println("Error: "+ex.getMessage());
+        }
+
+        close();
+    }
+
+    public static void anadirProducto(Producto[] prod){
+        connect();
+
+        try{
+            for(int i=0; i<prod.length; i++){
+                conexion.prepareStatement("insert into Ticket values('"+prod[i].getReferenciaProducto()+"'"
+                        +","+"'"+prod[i].getNombreProducto()+"'"+","+"'"+prod[i].getReferenciaPrecio()+"'"+");");
+            }
+        }catch(SQLException ex){
+            System.out.println("Error: "+ex.getMessage());
+        }
+
+        close();
+    }
+
+    public static void anadirVentas(Ventas[] ven){
+        connect();
+
+        try{
+            for(int i=0; i<ven.length; i++){
+                conexion.prepareStatement("insert into Ticket values('"+ven[i].getNumeroVenta()+"'"
+                        +","+"'"+ven[i].getReferenciaProducto()+"'"+","+"'"+ven[i].getCantidade()+"'"+");");
+            }
+        }catch(SQLException ex){
+            System.out.println("Error: "+ex.getMessage());
+        }
+
+        close();
+    }
+
+    public static void anadirTicket(Ticket[] array_ticket){
+        connect();
+        try{
+            for(int i=0;i<array_ticket.length;i++){
+            st=conexion.prepareStatement("insert into Ticket values('"+array_ticket[i].getNumeroVenta()+"'"
+                    +","+"'"+array_ticket[i].getNombre()+"'"
+                    +","+"'"+array_ticket[i].getPrecioTotal()+"'"+");");
+            st.execute();
+            }
+        }catch(SQLException ex){
+            System.out.println("Error:"+ex.getMessage());
+        }
+        close();
+    }
+
     public static void crear(Ventas array_ventas[], Producto array_productos[], Precio array_precios[], Ticket array_ticket[]){
 
-        for(int i=0;i<array_ventas.length;i++){
+        for(int i=0; i<array_ventas.length; i++){
             numeroVenta=array_ventas[i].getNumeroVenta();
             array_ticket[i].setNumeroVenta(numeroVenta);
             referenciaProducto=array_ventas[i].getReferenciaProducto();
             cantidad=array_ventas[i].getCantidade();
-            for(int u=0;u<array_productos.length;u++){
+            for(int u=0; u<array_productos.length; u++){
                 if(referenciaProducto.equals(array_productos[u].getReferenciaProducto())){
                     nombre=array_productos[u].getNombreProducto();
                     referenciaPrecio=array_productos[u].getReferenciaPrecio();
                     array_ticket[i].setNombre(nombre);
-                    for(int x=0;x<array_precios.length;x++){
+                    for(int x=0; x<array_precios.length; x++){
                         if(referenciaPrecio.equals(array_precios[x].getReferenciaPrecio())){
                             precio=array_precios[x].getPrecio();
                             array_ticket[i].setPrecioTotal(precio*cantidad);
@@ -43,20 +124,20 @@ public class MetodosArray{
             }
             System.out.println("Ticket "+(i+1)+": "+(array_ticket[i].toString()));
         }
-    }  
+    }
 
     public static void nombrePrecio(Ventas array_ventas[], Producto array_productos[], Precio array_precios[]){
-        
+
         numeroVenta=JOptionPane.showInputDialog("inserte número venta:");
-        
-        for(int i=0;i<array_ventas.length;i++){
+
+        for(int i=0; i<array_ventas.length; i++){
             if(numeroVenta.equals(array_ventas[i].getNumeroVenta())){
                 referenciaProducto=array_ventas[i].getReferenciaProducto();
-                for(int u=0;u<array_productos.length;u++){
-                     if(referenciaProducto.equals(array_productos[u].getReferenciaProducto())){
+                for(int u=0; u<array_productos.length; u++){
+                    if(referenciaProducto.equals(array_productos[u].getReferenciaProducto())){
                         nombre=array_productos[u].getNombreProducto();
                         referenciaPrecio=array_productos[u].getReferenciaPrecio();
-                        for(int x=0;x<array_precios.length;x++){
+                        for(int x=0; x<array_precios.length; x++){
                             if(referenciaPrecio.equals(array_precios[x].getReferenciaPrecio())){
                                 precio=array_precios[x].getPrecio();
                             }
